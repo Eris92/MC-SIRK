2198b0c3cd713eb2bd65308ca4eff1d33bbed4fc
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-10 19:48:05 +0200

    feat(admin): bind secure integration admin policy

--- SIRKPortalAdmin.js
+++ SIRKPortalAdmin.js
@@ -1,3 +1,8 @@
 "use strict";
 
-module.exports = require("./admin.js");
+var implementation = require("./admin.js");
+var integrationAdmin = require("./server/core/integration-admin-policy.js");
+
+module.exports.admin = function (plugin) {
+    return integrationAdmin.wrap(implementation.admin(plugin), plugin);
+};
