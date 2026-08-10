74537a572d872f39549424594bc60180e4eb79dd
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-10 19:58:51 +0200

    fix(admin): preserve canonical admin entrypoint delegation

--- SIRKPortalAdmin.js
+++ SIRKPortalAdmin.js
@@ -1,8 +1,3 @@
 "use strict";
 
-var implementation = require("./admin.js");
-var integrationAdmin = require("./server/core/integration-admin-policy.js");
-
-module.exports.admin = function (plugin) {
-    return integrationAdmin.wrap(implementation.admin(plugin), plugin);
-};
+module.exports = require("./admin.js");
