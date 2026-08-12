f1aca6fbd3872a34430cc777fd65fcaf8a1da7e7
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-12 22:59:32 +0200

    Narrow Jira cache reference graph to Users objects

--- seed/MyScripts/Jira/Jira Cache Assets.ps1
+++ seed/MyScripts/Jira/Jira Cache Assets.ps1
@@ -4,7 +4,7 @@
 # VariableSwitch: $Force, false, Wymuś odświeżenie|
 # SirkWorkflow: JiraAssetsCache
 # SirkSystemCredential: Jira
-# SirkJiraAssetAql: objectType in objectTypeAndChildren("Sprzęt użytkownika") OR object HAVING outboundReferences() OR object HAVING inboundReferences()
+# SirkJiraAssetAql: objectType in objectTypeAndChildren("Sprzęt użytkownika") OR objectType = "Users" OR object HAVING outboundReferences(objectType = "Users") OR object HAVING inboundReferences(objectType = "Users")
 # SirkJiraAssetLabelAttribute: Nazwa_sieciowa
 # MultiHost: false
 # runAsUser: 0
