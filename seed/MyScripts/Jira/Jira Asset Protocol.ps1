337419486ef2c45734d3423c8925bb05b8981327
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-12 22:59:23 +0200

    Narrow Jira reference graph to Users objects

--- seed/MyScripts/Jira/Jira Asset Protocol.ps1
+++ seed/MyScripts/Jira/Jira Asset Protocol.ps1
@@ -7,7 +7,7 @@
 # VariableUserRequired: $ItPerson, Osoba IT|Lista użytkowników MeshCentral; domyślnie aktualnie zalogowany operator.
 # SirkWorkflow: JiraAssetProtocol
 # SirkSystemCredential: Jira
-# SirkJiraAssetAql: objectType in objectTypeAndChildren("Sprzęt użytkownika") OR object HAVING outboundReferences() OR object HAVING inboundReferences()
+# SirkJiraAssetAql: objectType in objectTypeAndChildren("Sprzęt użytkownika") OR objectType = "Users" OR object HAVING outboundReferences(objectType = "Users") OR object HAVING inboundReferences(objectType = "Users")
 # SirkJiraAssetLabelAttribute: Nazwa_sieciowa
 # SirkJiraAssetUserVariable: JiraUser
 # MultiHost: false
